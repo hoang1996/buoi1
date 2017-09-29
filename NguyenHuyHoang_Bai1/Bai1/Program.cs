abb6f20a96a70e3a81f5b569594d60473afd7b9d
DESKTOP-3AJM04N\admin <14dh110113@gmail.com>
2017-09-29 22:54:20 +0700

    PhanTichIP

---
+++ NguyenHuyHoang_Bai1/Bai1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+namespace Bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String strHostName = Dns.GetHostName();
+            Console.WriteLine("Host Name: " + strHostName);
+
+            IPHostEntry iphostentry = Dns.GetHostByName(strHostName);
+
+            int nIP = 0;
+            foreach (IPAddress ipaddress in iphostentry.AddressList)
+            {
+                Console.WriteLine("IP #" + ++nIP + ": " + ipaddress.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
